1241626b260a128f3133119129b973bd649ddec8
Vignesh Balasubramaniam <vigneshb.hp@gmail.com>
2021-10-24 18:50:58 -0400

    Cleanup container entrypoint variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-if [[ -n "$ENV" && -n "$PORT" ]]; then
+if [[ -n "$ENV" ]]; then
     if ! [[ "$ENV" == "production" || "$ENV" == "development" ]]; then
         echo "Invalid environment"
     else
-        hugo server --baseURL https://www.viggy96.me/ --environment $ENV --bind 0.0.0.0 --port $PORT
+        if [[ "$ENV" == "production" ]]; then
+            hugo server --baseURL https://www.viggy96.me/ --environment $ENV --bind 0.0.0.0 --port 443
+        elif [[ "$ENV" == "development" ]]; then
+            hugo server --baseURL http://localhost --environment $ENV --bind 0.0.0.0 --port 8080
+        fi
     fi
 else
     echo "Required environment variables missing"
